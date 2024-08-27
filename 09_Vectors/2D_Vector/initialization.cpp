81e397863526537422f7c4f20967063257e92110
mini-360 <rajabhay56999@gmail.com>
2024-08-27 12:08:48 +0530

    pascal triangle question

---
+++ 09_Vectors/2D_Vector/initialization.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int m, n;
+    cout << "Enter value of m and n : ";
+    cin >> n >> m;
+    vector<vector<int>> vec(n,vector<int>(m));
+    return 0;
+}
